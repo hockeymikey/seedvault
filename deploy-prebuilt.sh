133c5b936d2e09cf3d90bdb20d90c35b993639e2
hockeymikey <hockeymikey@hockeymikey.com>
2020-09-30 18:38:52 -0500

    Jenkins building

---
+++ deploy-prebuilt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Script to deploy to a prebuilt repo.
+
+REPO_URL="https://hockeymikey:$GITHUB_API_KEY@github.com/hockeymikey/seedvault-prebuilt"
+TAG=$(git tag -l --points-at HEAD)
+BRANCH=$GIT_BRANCH
+
+git config --global user.email "hockeymikey@hockeymikey.com"
+git config --global user.name "hockeymikey"
+git clone --quiet $REPO_URL
+
+cd seedvault-prebuilt
+git checkout $BRANCH || git checkout -b $BRANCH
+rm -Rf ./*
+cp $WORKSPACE/Android.mk.prebuilt ./Android.mk
+cp $WORKSPACE/app/build/outputs/apk/release/app-release-unsigned.apk ./Seedvault.apk
+cp $WORKSPACE/permissions_com.stevesoltys.seedvault.xml .
+cp $WORKSPACE/whitelist_com.stevesoltys.seedvault.xml .
+
+git add .
+git commit -m "Jenkins build $BUILD_NUMBER" -m "https://github.com/hockeymikey/seedvault/commit/$GIT_COMMIT "
+git push origin $BRANCH
+
+if [ ! -z ${TAG} ]; then
+	git tag ${TAG}
+	git push origin --tags
+fi
